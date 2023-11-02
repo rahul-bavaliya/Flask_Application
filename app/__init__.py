bfdd0856dbd456e4da82cf09e055453641b04846
bavaliya5659-spt <rahul.bavaliya.jobs@gmail.com>
2023-11-02 14:52:06 -0600

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('./templates/index.html')
 
 if __name__ == '__main__':
     app.run()
