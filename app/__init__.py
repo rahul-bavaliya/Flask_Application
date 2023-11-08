10393e390faddae22bad6d28f792f53fe25d409a
bavaliya5659-spt <rahul.bavaliya.jobs@gmail.com>
2023-11-08 13:56:43 -0600

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
